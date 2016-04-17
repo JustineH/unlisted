b02d4ca0339066080a38803046d3e5bbec306007
Jules Wei <s.jules2000@gmail.com>
2016-04-17 03:14:11 +0000

    edit models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
   has_many :listings
+  has_many :bookmarks
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417002245) do
+ActiveRecord::Schema.define(version: 20160417002848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: true do |t|
+    t.boolean  "favourite",  default: false
+    t.integer  "user_id"
+    t.integer  "listing_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "bookmarks", ["listing_id"], name: "index_bookmarks_on_listing_id", using: :btree
+  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id", using: :btree
 
   create_table "listings", force: true do |t|
     t.integer  "user_id"
