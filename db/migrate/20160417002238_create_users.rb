553c6ef6693a267ff648331839a69dfb67a6d115
Jules Wei <s.jules2000@gmail.com>
2016-04-17 00:24:54 +0000

    edit listing model

--- db/migrate/20160417002238_create_users.rb
+++ db/migrate/20160417002238_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-
+     create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.integer :phone
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.boolean :seller, default: false
+      
       t.timestamps
     end
   end
--- db/migrate/20160417002245_create_listings.rb
+++ db/migrate/20160417002245_create_listings.rb
@@ -1,6 +1,56 @@
 class CreateListings < ActiveRecord::Migration
   def change
-    create_table :listings do |t|
+   create_table :listings do |t|
+      t.belongs_to :user, index: true 
+
+      Address
+      CityArea
+      t.integer :numOfBed, default: 0 
+      t.integer :numOfBath, default: 0 
+      t.integer :sqft, default: 0 
+      t.integer YearBuilt
+      HomeType
+      TypeOfHome
+      TypeOwnership
+      # Size
+      t.integer :yearBuilt
+      t.integer :age
+      t.integer :stories
+      t.integer :levels, default: 0 
+      t.integer :bedrooms, default: 0 
+      t.integer :fullBathrooms, default: 0 
+      t.integer :halfBathrooms, default: 0 
+      t.integer :fireplaces, default: 0 
+      t.integer :taxes, default: 0 
+      t.integer :taxYear
+      t.integer :strataFees, default: 0 
+      lotSize(sq ft)
+      # Display: acres, sq m, hectares
+      t.integer :walkscore, default: 0 
+      t.string :extraFeaturesDescription
+      #Condo Extras
+      t.integer :parking, default: 0 
+      t.integer :storage, default: 0 
+
+      # Amenities
+      t.boolean :bikeRoom, default: false
+      t.boolean :elevator, default: false
+      t.boolean :garden, default: false
+      t.boolean :inSuiteLaundry, default: false
+      t.boolean :garage, default: false
+      t.boolean :doorman, default: false
+      t.boolean :gym, default: false
+      t.boolean :amenitiesRoom, default: false
+      # Other
+      t.boolean :airconditioning
+      t.boolean :washer, default: false
+      t.boolean :dishwasher, default: false
+      t.boolean :stove, default: false
+      t.boolean :fridge, default: false
+      t.boolean :hotTub, default: false
+      t.boolean :pool, default: false
+      t.boolean :petFriendly, default: false
+      t.boolean :wheelchairAccessible, default: false
 
       t.timestamps
     end
