2be26d27ad57159f334cc2759f3bbab7c2a193ea
Justine Herman <justine.m.h@gmail.com>
2016-04-17 02:02:55 +0000

    Update Bookmark migration

--- db/migrate/20160417002848_create_bookmarks.rb
+++ db/migrate/20160417002848_create_bookmarks.rb
@@ -1,8 +1,12 @@
 class CreateBookmarks < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
-
-      t.timestamps
+      t.boolean :favourite, default: false
+      t.references :user, index: true, foreign_key: true
+      t.references :listing, index: true, foreign_key: true
+      t.timestamps 
     end
   end
 end
+
+
