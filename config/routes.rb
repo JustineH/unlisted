89880ede8b70d60d84e1245723295060c1086ac8
kytorini <khung@me.com>
2016-04-17 01:58:33 +0000

    add bootstrape

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Unlisted::Application.routes.draw do
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
-  get 'home/index'
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
 
-
-  root "home#index"
 end
