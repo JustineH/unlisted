135bedf90fc20c3841c6554fbd96074ea1c5717b
kytorini <khung@me.com>
2016-04-20 23:49:36 +0000

    add guest feature in application erb, user controller and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ActiveRecord::Base
   has_many :listings
   has_many :bookmarks
 
+  def self.new_guest
+    new { |u| u.guest = true }
+  end
+  
+  def name
+    guest ? "Guest" : username
+  end
+  
+  def move_to(user)
+    tasks.update_all(user_id: user.id)
+  end
 
 end
