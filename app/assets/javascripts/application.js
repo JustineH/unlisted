3314b4c72651371502693c6e9af3450d5eba624c
kytorini <khung@me.com>
2016-04-20 20:57:09 +0000

    add comments for js, edit listing look

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,9 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+
+// Top nav bar behaviour
+// Creates bg and font colour when scrolled
 $(function(){
 if ($('.home-page').length > 0){
     window.addEventListener("scroll", function() {
@@ -57,12 +60,13 @@ if ($('.home-page').length > 0){
     },false);
 }
 
-
+// Home page carousel
+// rotate every 4 sec
 $('#welcome-carousel.carousel').carousel({
   interval: 4000
 });
 
-
+// Pause all other carousel
 $('.carousel').carousel({
   interval: false
 });
