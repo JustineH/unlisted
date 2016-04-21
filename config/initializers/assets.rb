25fc74962c8826801a1b6360faccea8601145ff6
kytorini <khung@me.com>
2016-04-21 21:03:41 +0000

    resolve css issues, manually config asset pipeline to reflect css per view

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,9 +3,17 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( unlisted_custom.css )
-
-
 # Rails.application.config.assets.precompile += %w( carousel.css )
+
+# Adding individual css according to view
+Rails.application.config.assets.precompile += %w( 
+welcome.css
+listings.css
+users.css
+vendors.css
+ )
+
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
