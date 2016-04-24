dff241cd242820246a7d18e5a265111497dea394
Jules Wei <s.jules2000@gmail.com>
2016-04-24 19:02:19 +0000

    edit show page with paperclip

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,6 @@ class Listing < ActiveRecord::Base
   belongs_to :neighbourhood
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   
+  # validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
