9562cbb0a37abbc787d63c22ab2ed18fe588beaa
cmm21 <chris.m.milbourne@gmail.com>
2016-04-27 02:20:23 +0000

    prepare for merge

--- app/assets/javascripts/form-wizard.js
+++ app/assets/javascripts/form-wizard.js
@@ -1,31 +1,37 @@
 $(document).on('click', 'a#to-details', function () {
     $('#details-page').show();
     $('#basic-page').hide();
+    $('a#detail-side-click').parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('click', 'a#back-to-basic', function(){
   $('#details-page').hide();
   $('#basic-page').show();
+  $('a#basic-side-click').parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('click', 'a#to-amenities', function(){
   $('#details-page').hide();
   $('#amenities-page').show();
+  $('a#amenity-side-click').parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('click', 'a#back-to-details', function(){
   $('#details-page').show();
   $('#amenities-page').hide();
+  $('a#detail-side-click').parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('click', 'a#to-photos', function(){
   $('#amenities-page').hide();
   $('#photos-page').show();
+  $('a#photo-side-click').parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('click', 'a#back-to-amenities', function(){
   $('#photos-page').hide();
   $('#amenities-page').show();
+  $('a#amenity-side-click').parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('keyup', 'input.address-form', function(){
@@ -45,6 +51,7 @@ $(document).on('click', 'a#basic-side-click', function(){
   $('#details-page').hide();
   $('#amenities-page').hide();
   $('#photos-page').hide();
+  $(this).parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('click', 'a#detail-side-click', function(){
@@ -52,6 +59,7 @@ $(document).on('click', 'a#detail-side-click', function(){
   $('#basic-page').hide();
   $('#amenities-page').hide();
   $('#photos-page').hide();
+  $(this).parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('click', 'a#amenity-side-click', function(){
@@ -59,6 +67,7 @@ $(document).on('click', 'a#amenity-side-click', function(){
   $('#details-page').hide();
   $('#basic-page').hide();
   $('#photos-page').hide();
+  $(this).parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
 $(document).on('click', 'a#photo-side-click', function(){
@@ -66,5 +75,6 @@ $(document).on('click', 'a#photo-side-click', function(){
   $('#amenities-page').hide();
   $('#details-page').hide();
   $('#basic-page').hide();
+  $(this).parent().addClass('side-nav-clicked').siblings().removeClass('side-nav-clicked');
 });
 
