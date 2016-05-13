37c99738504d4be56fdc547b7d42df526d270d7e
kytorini <khung@me.com>
2016-05-13 20:58:14 +0000

    attempt 2

--- config/initializers/_bonsai.rb
+++ config/initializers/_bonsai.rb
@@ -5,4 +5,4 @@
 
 
 
-Elasticsearch::Model.client = Elasticsearch::Client.new url: 'https://5w3w0atn:aj6weajxd4qsq0kx@privet-3653054.us-east-1.bonsai.io', log: true
+Elasticsearch::Model.client = Elasticsearch::Client.new url: BONSAI_URL, log: true
