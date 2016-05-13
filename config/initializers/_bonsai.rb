abebdf8b8a9a49cf19bc5713c0d8dd3a38bf08de
kytorini <khung@me.com>
2016-05-13 20:43:44 +0000

    hardcode bonsai URL

--- config/initializers/_bonsai.rb
+++ config/initializers/_bonsai.rb
@@ -1,1 +1,4 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL'], log: true
+# Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL'], log: true
+
+
+Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['https://5w3w0atn:aj6weajxd4qsq0kx@privet-3653054.us-east-1.bonsai.io'], log: true
