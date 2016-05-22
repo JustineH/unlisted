e85db30b8d35e2458683b72176a324b8061c1c50
Justine <justine.m.h@gmail.com>
2016-05-22 01:21:07 -0700

    Delete Elasticsearch folder

--- config/initializers/_bonsai.rb
+++ config/initializers/_bonsai.rb
@@ -1,8 +1,1 @@
 # Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL'], log: true
-
-
-# Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['https://5w3w0atn:aj6weajxd4qsq0kx@privet-3653054.us-east-1.bonsai.io'], log: true
-
-
-
-# Elasticsearch::Model.client = Elasticsearch::Client.new url: 'BONSAI_URL', log: true
